b55a86a6d1555ba3847e6f5d29a97398028a30fc
AbdoFathy11 <abdofathy100100@gmail.com>
2023-01-22 03:01:28 -0800

    added items to about section then styled them then added some animation in js file for them

--- js/main.js
+++ js/main.js
@@ -12,9 +12,23 @@
 /**
  * End of global variables
  */
+const leftItems = document.querySelectorAll('.left .item')
+const rightItems = document.querySelectorAll('.right .item')
 
+function aboutUsAnimation() {
 
+    leftItems.forEach(i => i.style = `transform: translate(-600px)`)
+    rightItems.forEach(i => i.style = `transform: translate(600px)`)
 
+}
+document.body.onscroll = () => {
+    if (scrollY >= 300) {
+        leftItems.forEach(i => i.style = `transform: translate(0)`)
+        rightItems.forEach(i => i.style = `transform: translate(0)`)
+    } else {
+        aboutUsAnimation()
+    }
+}
 
 
 
