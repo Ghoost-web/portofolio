e80867a81fa4c4934cfafac097955375d20b6679
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-01-31 21:49:43 +0200

    Edit the js

--- js/main.js
+++ js/main.js
@@ -41,14 +41,14 @@ document.body.onscroll = () => {
 function sideMenu() {
 	mobileMenu.style.display = 'block';
 	document.body.style.overflowY = 'hidden';
-}
+};
 // End the function for opining the mobile-menu 
 
 // Start the function for closing the mobile-menu
 function closeMenu() {
 	mobileMenu.style.display = 'none';
 	document.body.style.overflowY = 'visible';
-}
+};
 // End the function for closing the mobile-menu
 
 
@@ -59,6 +59,29 @@ x.onclick = function () {
     scroll({ top: 0, behavior: "smooth" });
 };
 
+window.onscroll = function () {
+    //console.log(scrollY);
+    if (scrollY >= 700) {
+      x.style.display = "block";
+    }  else if(scrollY >= 300) {
+        leftItems.forEach(i => i.style = `transform: translate(0)`);
+        rightItems.forEach(i => i.style = `transform: translate(0)`);
+    } else {
+        x.style.display = "none";
+        aboutUsAnimation();
+    }
+    /* start check the screeen in mobile mode */
+    if(theBody.getBoundingClientRect().width<2775){
+        skillsBoxs.forEach((box,i)=>{
+            if(box.getBoundingClientRect().top-window.innerHeight+100<1&&box.getBoundingClientRect().top-window.innerHeight+10>-500){
+               if(!arrEnBox[i]){
+                arrEnBox[i]=true;
+                box.style.right=`0px`;
+               }
+            }
+        })        
+    }
+  };
 
 /** end button an scroll  */
 
